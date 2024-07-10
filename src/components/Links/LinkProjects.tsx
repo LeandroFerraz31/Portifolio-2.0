f430a1c0bf10dd22d9b247e56bc7bae2c1a77d03
LeandroFerraz31 <leandro.santos@universo.univates.br>
2024-07-09 22:05:42 -0300

    Feat:Link projects components ajusteds by footer

---
+++ src/components/Links/LinkProjects.tsx
@@ -0,0 +1,33 @@
+import { Container, IconButton, Box, Link, Typography } from '@mui/material';
+import { Email, LinkedIn, GitHub } from '@mui/icons-material';
+
+const Footer: React.FC = () => {
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ display: 'flex', justifyContent: 'center', mb: 2 }}>
+        <Link href="https://github.com/LeandroFerraz31" target="_blank" rel="noopener" color="inherit">
+          <IconButton sx={{ mx: 5, '&:hover': { transform: 'scale(1.2)' } }}>
+            <GitHub sx={{ fontSize: 40 }} />
+          </IconButton>
+        </Link>
+        <Link href="https://www.linkedin.com/in/leandro-fz" target="_blank" rel="noopener" color="inherit">
+          <IconButton sx={{ mx: 5, '&:hover': { transform: 'scale(1.2)' } }}>
+            <LinkedIn sx={{ fontSize: 40 }} />
+          </IconButton>
+        </Link>
+        <Link href="mailto:leandro.santos@universo.univates.br" color="inherit">
+          <IconButton sx={{ mx: 5, '&:hover': { transform: 'scale(1.2)' } }}>
+            <Email sx={{ fontSize: 40 }} />
+          </IconButton>
+        </Link>
+      </Box>
+      <Typography variant="body2" color="text.secondary" align="center">
+        {'Todos os direitos reservados Leandro Ferraz © '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    </Container>
+  );
+}
+
+export default Footer;
